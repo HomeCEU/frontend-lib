2407064403a805d770a98e9e012ca56f3927a0a1
Steve Giles <stephan.giles@gmail.com>
2020-10-20 17:16:15 -0400

    Rework unit test to validate grid data.

--- src/app/dts/dts.component.spec.ts
+++ src/app/dts/dts.component.spec.ts
@@ -13,6 +13,15 @@ describe('DtsComponent', () => {
   let fixture: ComponentFixture<DtsComponent>;
   let dtsService: DtsService;
 
+  /**
+   * Create the component which calls the constructor to populate the data grid
+   */
+  function createComponent(): void {
+    fixture = TestBed.createComponent(DtsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -32,41 +41,31 @@ describe('DtsComponent', () => {
     .compileComponents();
 
     dtsService = TestBed.get(DtsService);
-    spyOn(dtsService, 'getTemplates').and.returnValue(of(null));
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DtsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    spyOn(dtsService, 'getTemplates').and.returnValue(of(null));
+    createComponent();
     expect(component).toBeTruthy();
   });
 
-  it('should display a list of all available templates', done  => {
-    // reset the spy and expected result
+  it('should display a list of all available templates', ()  => {
     dtsService.getTemplates = jasmine.createSpy().and.returnValue(of(templatesAll.items));
+    createComponent();
 
-    fixture.detectChanges();
-
-    dtsService.getTemplates().subscribe( result => {
-      expect(result.length).toEqual(3);
-      done();
-    });
+    const gridData = fixture.debugElement.nativeElement.querySelectorAll('.datatable-body');
+    expect(gridData[0].textContent).toEqual('Robert MartinNutritionTemplate3fa85f64-5717-4562-b3fc-2c963f66afa62020-03-05T23:' +
+      '35:12.876ZSteve GilesPhysicalTherapyTemplate1fa85f64-5717-4562-b3fc-2c963f66afa62020-04-05T23:35:12.876ZSue AndersonNursingTem' +
+      'plate2fa85f64-5717-4562-b3fc-2c963f66afa62020-01-05T23:35:12.876Z');
   });
 
-  it('should display a list of templates by document type', done  => {
-    // reset the spy and expected result
+  it('should display a list of templates by document type', ()  => {
     dtsService.getTemplates = jasmine.createSpy().and.returnValue(of(templatesEnrollment.items));
+    createComponent();
 
-    fixture.detectChanges();
-
-    dtsService.getTemplates().subscribe( result => {
-      expect(result.length).toEqual(2);
-      done();
-    });
+    const gridData = fixture.debugElement.nativeElement.querySelectorAll('.datatable-body');
+    expect(gridData[0].textContent).toEqual('Robert MartinNutritionTemplate3fa85f64-5717-4562-b3fc-2c963f66afa62020-03-05T23:' +
+      '35:12.876ZSteve GilesPhysicalTherapyTemplate1fa85f64-5717-4562-b3fc-2c963f66afa62020-04-05T23:35:12.876Z');
   });
 
 });
