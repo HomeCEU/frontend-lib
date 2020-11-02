a85fca42ae3dcbaeb83d5cd43527eab2c36d8409
Steve Giles <stephan.giles@gmail.com>
2020-11-02 11:28:19 -0500

    Fix unit test

--- src/app/dts/dts.component.spec.ts
+++ src/app/dts/dts.component.spec.ts
@@ -7,7 +7,7 @@ import {NgxDatatableModule} from '@swimlane/ngx-datatable';
 import {templatesAll, templatesEnrollment} from '../../test/templates';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
 
 describe('DtsComponent', () => {
   let component: DtsComponent;
@@ -27,7 +27,8 @@ describe('DtsComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         NgxDatatableModule,
-        MatDialogModule
+        MatDialogModule,
+        ReactiveFormsModule
       ],
       declarations: [
         DtsComponent
@@ -56,8 +57,8 @@ describe('DtsComponent', () => {
     createComponent();
 
     const gridData = fixture.debugElement.nativeElement.querySelectorAll('.datatable-body');
-    expect(gridData[0].textContent).toEqual('NursingTemplateSue Anderson 1/5/20, 6:35 PM NutritionTemplateRobert Martin ' +
-      '3/5/20, 6:35 PM ');
+    expect(gridData[0].textContent).toEqual('NursingTemplateSue Anderson 1/5/20, 6:35 PM NutritionTemplateRobert Martin 3/5/20,' +
+      ' 6:35 PM PhysicalTherapyTemplateSteve Giles 4/5/20, 7:35 PM ');
   });
 
   it('should display a list of templates by document type', ()  => {
