00e75e8c4c83a645a260ab953eaab8619d84e35a
Steve Giles <stephan.giles@gmail.com>
2021-02-04 10:38:36 -0500

    Deploy to DOCK CEMS 2217

---
+++ src/test/document_template.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Test file for consuming the Angular frontend dts component in a PHP application (i.e. CEMS)
+ *
+ * To manually test this code locally without CEMS:
+ * - In your local webroot (i.e. Apache Tomcat), create the folder \bundles\cemscore\frontend
+ * - From frontend-lib/elements copy
+ * - - assets to \bundles\cemscore\frontend\assets
+ * - - styles.css and data-table.woff to \bundles\cemscore\frontend\css
+ * - - frontend-lib.js to \bundles\cemscore\frontend\script
+ * - Copy this file to any folder in your webroot
+ * - - delete all the php code
+ * - - replace line 39 with 40
+ * - Open this file in your webrowser
+ *
+ */
+
+  $dtsUrl = "http://dts-qa.us-west-2.elasticbeanstalk.com";
+  class User
+  {
+    var $userName;
+
+    public function __construct(string $name)
+    {
+      $this->userName = $name;
+    }
+  }
+  $user = new User("Test User");
+?>
+
+<!doctype>
+<html>
+  <head>
+    <title>Certificate Template</title>
+    <link rel="stylesheet" href="/bundles/cemscore/frontend/css/styles.css">
+    <script type="text/javascript" src="/bundles/cemscore/frontend/assets/ckeditor/ckeditor.js"></script>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <!--app-dts userName={{ user.username }} dtsUrl={{ dtsUrl }}></app-dts> -->
+    <app-dts userName = <?php echo $user->userName;?>  dtsUrl = <?php echo $dtsUrl;?> </app-dts>
+    <script type="text/javascript" src="/bundles/cemscore/frontend/scripts/frontend-lib.js"></script>
+  </body>
+</html>
