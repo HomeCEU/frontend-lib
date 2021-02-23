47fb1723e8041b75f0bd83bbad91e410676974f7
Steve Giles <stephan.giles@gmail.com>
2021-02-23 12:25:06 -0500

    Fix filter reset 2266

--- src/assets/ckeditor/config.js
+++ src/assets/ckeditor/config.js
@@ -8,16 +8,20 @@ CKEDITOR.editorConfig = function( config ) {
   config.removePlugins = 'exportpdf';
 
   config.toolbar = [
-    { name: 'basicstyles', items: [ 'Bold', 'Italic' ] },
-    { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', '-', 'Undo', 'Redo' ] },
-    { name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
-    { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
     { name: 'document', items: ['Source'] },
+    { name: 'clipboard', items: [ 'Cut', 'Copy', '-', 'Undo', 'Redo' ] },
+    { name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+    { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', '-', 'CopyFormatting', 'RemoveFormat'  ] },
+    { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
+
     '/',
     { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+    { name: 'colors', items: [ 'TextColor', 'BGColor' ] }
   ];
+  config.height = '60vh';
+  config.fullPage = true;
   config.startupMode = 'wysiwyg';
   config.allowedContent = true;
   config.fullPage = true;
-  config.height = '800px';
+  config.height = '60vh';
 };
